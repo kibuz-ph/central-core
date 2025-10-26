8a22fb6920142cf80e0f8dc27a923501f67bba14
alejor64 <alejoruizs@hotmail.com>
2025-10-25 23:27:16 -0500

    feat: verify if slug exists in db

--- src/ms-residential-complex/residential-complex/application/services/create-residential-complex.use-case.ts
+++ src/ms-residential-complex/residential-complex/application/services/create-residential-complex.use-case.ts
@@ -1,5 +1,6 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { generateSlug } from '../../../../common/utils/slug-generator.util';
+import { DomainException } from '../../../../modules/pino/domain/exceptions/domain.exception';
 import { ResidentialComplexInterface } from '../../domain/repositories/residential-complex.repository-interface';
 import { CreateResidentialComplexDto } from '../dto/create-residential-complex.dto';
 import { ResidentialComplexResponseDto } from '../dto/residential-complex-response.dto';
@@ -15,6 +16,16 @@ export class CreateResidentialComplexUseCase {
     createResidentialComplexDto: CreateResidentialComplexDto,
   ): Promise<ResidentialComplexResponseDto> {
     const slug = generateSlug(createResidentialComplexDto.name);
+    const residentialComplexExists = await this.residentialComplexInterface.findUnique({
+      conditions: { slug },
+    });
+
+    if (residentialComplexExists) {
+      throw new DomainException({
+        message: `Residential Complex with name: ${createResidentialComplexDto.name} already exists`,
+        statusCode: HttpStatus.CONFLICT,
+      });
+    }
 
     const residentialComplexCreated = await this.residentialComplexInterface.create({
       ...createResidentialComplexDto,
--- src/ms-residential-complex/residential-complex/application/services/update-residential-complex.use-case.ts
+++ src/ms-residential-complex/residential-complex/application/services/update-residential-complex.use-case.ts
@@ -1,9 +1,13 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { generateSlug } from '../../../../common/utils/slug-generator.util';
 import { DomainException } from '../../../../modules/pino/domain/exceptions/domain.exception';
 import { ResidentialComplexInterface } from '../../domain/repositories/residential-complex.repository-interface';
 import { ResidentialComplexResponseDto } from '../dto/residential-complex-response.dto';
 import { UpdateResidentialComplexDto } from '../dto/update-residential-complex.dto';
+
+interface DataToUpdateProps extends UpdateResidentialComplexDto {
+  slug?: string;
+}
 
 @Injectable()
 export class UpdateResidentialComplexUseCase {
@@ -24,12 +28,25 @@ export class UpdateResidentialComplexUseCase {
       throw new DomainException('Residential complex not found');
     }
     // If name is being updated, regenerate the slug
-    const dataToUpdate = updateResidentialComplex.name
+    const dataToUpdate: DataToUpdateProps = updateResidentialComplex.name
       ? {
           ...updateResidentialComplex,
           slug: generateSlug(updateResidentialComplex.name),
         }
       : updateResidentialComplex;
+
+    if (dataToUpdate?.slug) {
+      const residentialComplexExists = await this.residentialComplexInterface.findUnique({
+        conditions: { slug: dataToUpdate?.slug },
+      });
+
+      if (residentialComplexExists) {
+        throw new DomainException({
+          message: `Residential Complex with name: ${updateResidentialComplex.name} already exists`,
+          statusCode: HttpStatus.CONFLICT,
+        });
+      }
+    }
 
     const residentialComplex = await this.residentialComplexInterface.update(id, dataToUpdate);
     return ResidentialComplexResponseDto.fromEntities(residentialComplex);
